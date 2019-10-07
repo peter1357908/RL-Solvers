402171cbb435022680fe1e2123a54b692dd2521c
Peter Gao <peter1357908@hotmail.com>
2019-10-07 18:52:59 -0400

    Make the default demo TD3_BGGSE

--- BallGoalGravity-v0_TD3_viewer.py
+++ BallGoalGravity-v0_TD3_viewer.py
@@ -8,11 +8,11 @@ from stable_baselines import TD3
 if __name__ == "__main__":
     # TD3 does not allow for multi-processing
     # env = DummyVecEnv([lambda: gym.make('Self_Driving_Ball:BallGoalGravityEasy-v0')])
-    # env = DummyVecEnv([lambda: gym.make('Self_Driving_Ball:BallGoalGravityStopEasy-v0')])
-    env = DummyVecEnv([lambda: gym.make('Self_Driving_Ball:BallGoalGravity-v0')])  # despite being trained on Easy, the model will solve the regular difficulty!
+    env = DummyVecEnv([lambda: gym.make('Self_Driving_Ball:BallGoalGravityStopEasy-v0')])
+    # env = DummyVecEnv([lambda: gym.make('Self_Driving_Ball:BallGoalGravity-v0')])  # despite being trained on Easy, the model will solve the regular difficulty!
 
-    model = TD3.load("TD3_BGG")
-    # model = TD3.load("TD3_BGGSE")
+    # model = TD3.load("TD3_BGG")
+    model = TD3.load("TD3_BGGSE")
 
     # Enjoy trained agent
     obs = env.reset()
